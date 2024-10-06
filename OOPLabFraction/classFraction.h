de42c50bd5649ee524ba7fcb082a8224fba4129d
Zahar <Zahar@DESKTOP-0T3OVPN.local>
2024-10-06 21:54:28 +0700

    Описан класс Fraction: созданы конструкторы, геттеры, сеттеры, объявлены методы ввода/вывода дроби, арифм. операции и логические операторы в виде перегрузок и два собственных метода: запись дроби во float и возведение дроби в степень

---
+++ OOPLabFraction/classFraction.h
@@ -0,0 +1,53 @@
+#pragma once
+#include <iostream>
+#include <cstdlib>
+#include <stdexcept>
+
+
+class Fraction {
+
+    int numerator;
+    int denominator;
+
+public:
+    Fraction(); //default constructor
+    Fraction(int numerator = 1, int denominator = 1) {    // constructor with parameters
+        setNumerator(numerator);
+        setDenominator(denominator);  
+    };
+
+    int getNumerator() {  //getter 1 for numerator
+        return this -> numerator;
+    }
+
+    int getDenominator() {  //getter 2 for denominator
+        return this -> denominator;
+    }
+
+    void setNumerator(int numerator) {  //setter 1 for numerator
+        this -> numerator = numerator;
+
+    }
+
+    void setDenominator(int denominator) {   //setter for denominator
+        this -> denominator = denominator;
+    }
+
+
+    Fraction InputFraction() const;
+    void OutputFraction() const;
+    Fraction operator + (Fraction b);
+    Fraction operator - (Fraction b);
+    Fraction operator * (Fraction b);
+    Fraction operator / (Fraction b);
+    bool operator < (Fraction b);
+    bool operator > (Fraction b);
+    bool operator == (Fraction b);
+    bool operator != (Fraction b);
+    int gcd();
+    void Reduce();
+    float ConvertFraction() const;
+    Fraction operator ^ (int power);
+
+
+};
